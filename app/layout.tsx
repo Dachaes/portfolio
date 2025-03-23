02975d019cb731d07cb156d150567bf7d0411047
Dachae <no3yho2@naver.com>
2025-03-23 15:31:59 +0900

    init: 프로젝트 셋팅

---
+++ app/layout.tsx
@@ -0,0 +1,34 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+import Menu from "@/components/Menu";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Sohyeon Jeon",
+  description: "Sohyeon's Portfolio",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body>
+        <Menu />
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+// Icons from "https://lucide.dev/"
+
+export default function Home() {
+  return (
+    <div>
+    </div>
+  );
+}
